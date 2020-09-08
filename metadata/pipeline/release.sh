5aca7d3e3f41b39009283fa80058958dd4bcb19b
Veysel Kocaman <vkocaman@gmail.com>
2020-09-08 10:31:41 +0200

    synch jsl metadata

---
+++ metadata/pipeline/release.sh
@@ -0,0 +1,17 @@
+bucket_name=$1
+clinical_aws_key=$2
+aws_access_key=$3
+aws_access_secret=$4
+clinical_local_path=$5
+
+# Remove any existing versions of a ZIP
+#rm -rf $local_path
+
+# Create a zip of the current directory.
+#zip -r $local_path . -x .git/ .git/*** .github/workflows/release.yml scripts/pipeline/release.sh scripts/pipeline/upload_file_to_s3.py .DS_Store
+
+# Install required dependencies for Python script.
+pip3 install boto3
+
+# Run upload script
+python3 metadata/pipeline/upload_file_to_s3.py $bucket_name $clinical_aws_key $aws_access_key $aws_access_secret $clinical_local_path
