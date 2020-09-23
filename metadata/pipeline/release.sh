e14478c8318d0a596d15e2becbedb36b0675b619
Veysel Kocaman <vkocaman@gmail.com>
2020-09-23 10:46:58 +0200

    100d models [metadata]

--- metadata/pipeline/release.sh
+++ metadata/pipeline/release.sh
@@ -10,9 +10,10 @@ clinical_local_path=$5
 # Create a zip of the current directory.
 #zip -r $local_path . -x .git/ .git/*** .github/workflows/release.yml scripts/pipeline/release.sh scripts/pipeline/upload_file_to_s3.py .DS_Store
 
-apt-get install python3-setuptools
+#apt-get install python3-setuptools
+pip3 install -U pip setuptools 
 # Install required dependencies for Python script.
-pip install boto3
+pip3 install boto3
 #git clone git://github.com/boto/boto.git && cd boto && python3 setup.py install
 #cd ..
 
